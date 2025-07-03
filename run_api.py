6aad0fa62e9444f8c1d32c4d7d13be23947d887d
JoseStalker117 <josefriki23@gmail.com>
2025-07-03 13:48:11 -0600

    Puerto corregido y requerimientos actualizados

--- run_api.py
+++ run_api.py
@@ -9,7 +9,7 @@ def main():
     
     # Configuración del servidor
     host = os.getenv("HOST", "0.0.0.0")
-    port = int(os.getenv("PORT", 8801))
+    port = int(os.getenv("PORT", 8800))
     reload = os.getenv("RELOAD", "true").lower() == "true"
     
     print(f"🚀 Iniciando API de Transacciones MongoDB...")
